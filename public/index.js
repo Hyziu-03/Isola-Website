1ee33e4e2835866ff10aa6b155f35ab7887a4713
Szymon Hyziak <szymon.hyziak@gmail.com>
2022-09-14 17:51:37 +0200

    Deploying the site

--- public/index.js
+++ public/index.js
@@ -6,13 +6,13 @@ import {
 } from "https://www.gstatic.com/firebasejs/9.9.4/firebase-analytics.js";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAz4fw098wGRqlB_N5swvm8uObpeneMpkg",
-    authDomain: "isola-website.firebaseapp.com",
-    projectId: "isola-website",
-    storageBucket: "isola-website.appspot.com",
-    messagingSenderId: "742724889721",
-    appId: "1:742724889721:web:3dc85ffacdfc42b96bb8a8",
-    measurementId: "G-1HNZLXWLRB"
+    apiKey: "AIzaSyC6bxlow1yHkHpMSvpbmXiu76EXFNzD8Kw",
+    authDomain: "isolajs.firebaseapp.com",
+    projectId: "isolajs",
+    storageBucket: "isolajs.appspot.com",
+    messagingSenderId: "585967781854",
+    appId: "1:585967781854:web:68baa73c58964ae2a43f4f",
+    measurementId: "G-88X0G1ZVDW"
 };
 
 const app = initializeApp(firebaseConfig);
